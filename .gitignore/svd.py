bf436f5c46dc8772427171eeaa509fcb1dc9fc21
Rigel6091 <79652796+Rigel6091@users.noreply.github.com>
2021-02-25 22:07:39 +0530

    Update svd.py

--- .gitignore/svd.py
+++ .gitignore/svd.py
@@ -1,10 +1,23 @@
-
 import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.decomposition import TruncatedSVD
 import warnings
-In [2]:
 mov=pd.read_csv('movies.csv')
-In [3]:
 rat=pd.read_csv('ratings.csv')
+mov.head()
+rat.head()
+df=pd.merge(mov,rat,on="movieId")
+df.head()
+matrix=df.pivot_table(index='userId',columns='title',values='rating').fillna(0)
+X=matrix.values.T
+SVD=TruncatedSVD(n_components=20, random_state=0)
+matrixp=SVD.fit_transform(X)
+matrixp.shape
+corr = np.corrcoef(matrixp)
+title=matrix.columns
+title_list = list(title)
+rec = title_list.index('TV Set, The (2006)')
+corr_rec  = corr[rec]
+list(title[(corr_rec >= 0.9)])
+list(set(df["title"]))
